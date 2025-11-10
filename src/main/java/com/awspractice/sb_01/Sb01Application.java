1b82dd4badc06ac2fd3ea4d1a037e206de4cb1de
udaych-commits <udaychallagulla111@gmaile.com>
2025-11-10 18:19:23 +0530

    Initial commit

---
+++ src/main/java/com/awspractice/sb_01/Sb01Application.java
@@ -0,0 +1,13 @@
+package com.awspractice.sb_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/awspractice/sb_01/Sb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awspractice.sb_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
